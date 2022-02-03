d0383d4e586c1bcaa451ba80c3eedc84e8b26a14
Lok <lb.tamang77@gmail.com>
2022-02-03 16:00:21 -0700

    test

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-// Your JS goes here
+for (let i = 1; i < 64; i++) {
+    const div = document.createElement('div');
+    div.style.width = '11.1%';
+    div.style.float = 'left';
+    div.style.paddingBottom = '11.1%';
+    document.body.append(div);
+}
